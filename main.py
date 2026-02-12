4ebbc3247addbe8860d4404ac575e11b89725d2a
gslujeben1-web <g.slujeben.1@gmail.com>
2026-02-12 10:33:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ st.title("🟰Give the answer to each calculation➗")
 st.write("Whats your name:")
 name = st.text_input("*name*")
 
-st.write("find x:\n3x−7=2x+5")
-q1 = st.number_input("type the Answer")
+st.write("find x:")
+st.write("3x−7=2x+5")
+q1 = st.text_input("type the Answer")
 
 if q1 == 12:
   st.write("✅Correct✅")
 else:
-  st.write("❌Incorrect❌\nThe Right answer is 12")
+  st.write("❌Incorrect❌")
+  st.write("Correct answer is 12")
